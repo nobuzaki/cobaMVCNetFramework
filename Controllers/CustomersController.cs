70e43072bdbbbf2fa2753b3ec0efacc5636099f5
nobuzaki <48944472+nobuzaki@users.noreply.github.com>
2020-04-21 08:17:23 +0700

    Course44 - Update Data

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -36,12 +36,44 @@ namespace CobaMVCNetFramework.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+        public ActionResult Edit(int Id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+            
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
         }
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -6,12 +6,19 @@ namespace CobaMVCNetFramework.Models
     public class Customer
     {
         public int Id { get; set; }
+        
         [Required]
         [StringLength(255)]
         public String Name { get; set; }
+
+        
         public bool IsSubscribedToNewsletter { get; set; }
+        
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membersip Type")]
         public byte MembershipTypeId { get; set; }
+        
         [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
     }
